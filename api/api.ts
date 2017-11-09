fcbdb029aaa8f086c33e229efda6d9adad1c2061
Andre Guilherme <andre.guilherme@iatec.com>
2017-11-09 13:43:44 -0200

    Auto-generated commit

---
+++ api/api.ts
@@ -0,0 +1,9 @@
+export * from './PaycheckApi';
+import { PaycheckApi } from './PaycheckApi';
+export * from './ReimbursementApi';
+import { ReimbursementApi } from './ReimbursementApi';
+export * from './ReportsApi';
+import { ReportsApi } from './ReportsApi';
+export * from './VacationApi';
+import { VacationApi } from './VacationApi';
+export const APIS = [PaycheckApi, ReimbursementApi, ReportsApi, VacationApi];
